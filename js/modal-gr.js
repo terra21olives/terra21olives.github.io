70128ec32314301f4968b2c38c48d406f97a09a0
terra21olives <terra21olives@gmail.com>
2022-04-07 11:45:05 +0300

    Update modal-gr.js

--- js/modal-gr.js
+++ js/modal-gr.js
@@ -22,6 +22,18 @@ $(".pop").on("click", function (e) {
     $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
 });
 
+function zoom(e) {
+    var zoomer = e.currentTarget;
+    zoomer.ondragstart = function () {
+        return false;
+    };
+    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+    x = offsetX / zoomer.offsetWidth * 100
+    y = offsetY / zoomer.offsetHeight * 100
+    zoomer.style.backgroundPosition = x + '% ' + y + '%';
+}
+
 //center popup
 function centerModal() {
     $(this).css('display', 'block');
