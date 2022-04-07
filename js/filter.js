d486ece33f0d6a568fae774b1f8345c33adaf646
terra21olives <terra21olives@gmail.com>
2022-04-07 11:35:16 +0300

    drag zoom

--- js/filter.js
+++ js/filter.js
@@ -24,15 +24,6 @@ $(function () {
   }
 });
 
-function zoom(e) {
-  var zoomer = e.currentTarget;
-  e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
-  e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
-  x = offsetX / zoomer.offsetWidth * 100
-  y = offsetY / zoomer.offsetHeight * 100
-  zoomer.style.backgroundPosition = x + '% ' + y + '%';
-}
-
 //window.onload = activeAll();
 
 function activeAll() {
